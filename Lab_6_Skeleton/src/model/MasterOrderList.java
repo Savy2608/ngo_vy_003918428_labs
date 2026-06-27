5755a12924e5863f8afdf7490ff73fb6632e571d
Savy2608 <vyngo26082001@gmail.com>
2026-06-27 08:34:37 -0700

    Updated Lab 6

---
+++ Lab_6_Skeleton/src/model/MasterOrderList.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author vyngo
+ */
+   public class MasterOrderList {
+    
+    ArrayList<Order> orderlist;
+
+    public MasterOrderList() {        
+    orderlist  = new ArrayList<Order>();  
+    }
+
+    public ArrayList<Order> getOrderlist() {
+        return orderlist;
+    }
+
+    public void setOrderlist(ArrayList<Order> orderlist) {
+        this.orderlist = orderlist;
+    }
+
+    public void addNewOrder(Order order) {
+        this.orderlist.add(order);
+    }
+   }
+    
+   
+    
+
